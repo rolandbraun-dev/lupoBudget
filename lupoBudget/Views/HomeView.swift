eec3562a8d77853d89181263e9094b58218e892d
Roland Braun <mail@rolandbraun.com>
2020-06-30 17:55:15 +0200

    Initial commit

---
+++ lupoBudget/Views/HomeView.swift
@@ -0,0 +1,21 @@
+//
+//  HomeView.swift
+//  lupoBudget
+//
+//  Created by Roland Braun on 6/30/20.
+//  Copyright © 2020 Roland Braun. All rights reserved.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
---
+++ lupoBudget/Views/TabBarView.swift
@@ -0,0 +1,21 @@
+//
+//  TabBarView.swift
+//  lupoBudget
+//
+//  Created by Roland Braun on 6/30/20.
+//  Copyright © 2020 Roland Braun. All rights reserved.
+//
+
+import SwiftUI
+
+struct TabBarView: View {
+    var body: some View {
+        Text("Hello, TabBar")
+    }
+}
+
+struct TabBarView_Previews: PreviewProvider {
+    static var previews: some View {
+        TabBarView()
+    }
+}
