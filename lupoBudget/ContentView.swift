66ebdde18fa9b09067b230978bae5452cf685d46
Roland Braun <mail@rolandbraun.com>
2020-07-01 18:56:15 +0200

    Added different view, depending on the currentView-attribute

--- lupoBudget/ContentView.swift
+++ lupoBudget/ContentView.swift
@@ -10,14 +10,22 @@ import SwiftUI
 
 struct ContentView: View {
     
+    @State var currentView: String = "home"
+    
     var body: some View {
         
-        VStack {
+        VStack(spacing: 0) {
+            if self.currentView == "home" {
                 HomeView()
+            } else if self.currentView == "dollar" {
+                Text("DOLLAR")
+            } else if self.currentView == "settings" {
+                SettingsView()
+            }
                 
-                Spacer()
+            Spacer()
                 
-                TabBarView()
+            TabBarView(currentView: $currentView)
             }
             .edgesIgnoringSafeArea(.bottom)
         
@@ -31,7 +39,21 @@ struct ContentView_Previews: PreviewProvider {
 }
 
 enum SFSymbols {
-    static let home     = Image(systemName: "house")
-    static let gear     = Image(systemName: "gear")
-    static let dollarfill   = Image(systemName: "dollarsign.circle.fill")
+    static let home             = Image(systemName: "house")
+    static let gear             = Image(systemName: "gear")
+    static let dollar_fill      = Image(systemName: "dollarsign.circle.fill")
+    static let dollar           = Image(systemName: "dollarsign.circle")
+    static let slider           = Image(systemName: "slider.horizontal.3")
+    static let info             = Image(systemName: "info")
+    static let info_circle      = Image(systemName: "info.circle")
+    static let chevron_right    = Image(systemName: "chevron.right")
+}
+
+func collectLatestBookings() -> [String]{
+    var result: [String] = []
+    for currentNumber in 0...50 {
+        result.append(String(currentNumber))
+    }
+    
+    return result
 }
