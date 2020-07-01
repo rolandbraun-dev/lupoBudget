2b3e0f16f3a4bec54917b132b1109b148bc7390f
Roland Braun <mail@rolandbraun.com>
2020-07-01 18:56:34 +0200

    First commit

---
+++ lupoBudget/Views/SettingsView.swift
@@ -0,0 +1,21 @@
+//
+//  SettingsView.swift
+//  lupoBudget
+//
+//  Created by Roland Braun on 7/1/20.
+//  Copyright © 2020 Roland Braun. All rights reserved.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text("SettingsView")
+    }
+}
+
+struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsView()
+    }
+}
