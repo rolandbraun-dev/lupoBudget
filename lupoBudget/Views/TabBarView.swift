b30c33b545a318abfd22858af1a97bf0e506d159
Roland Braun <mail@rolandbraun.com>
2020-07-01 18:55:24 +0200

    Added tapgesture. A tap now switches to the called view.

--- lupoBudget/Views/TabBarView.swift
+++ lupoBudget/Views/TabBarView.swift
@@ -9,21 +9,37 @@
 import SwiftUI
 
 struct TabBarView: View {
+    
+    @Binding var currentView: String
+    
     var body: some View {
         
         HStack(spacing: 0) {
-                TabBarItemView(symbol: SFSymbols.home, label: "Home")
-                TabBarItemView(symbol: SFSymbols.dollarfill, label: "Dollar")
-                TabBarItemView(symbol: SFSymbols.gear, label: "Settings")
+            
+            TabBarItemView(symbol: SFSymbols.home, label: "Home", forView: "home", currentView: $currentView)
+                .onTapGesture {
+                    self.currentView = "home"
+                }
+            
+            TabBarItemView(symbol: SFSymbols.dollar, label: "Dollar", forView: "dollar", currentView: $currentView)
+                .onTapGesture {
+                     self.currentView = "dollar"
+                }
+            
+            TabBarItemView(symbol: SFSymbols.gear, label: "Settings", forView: "settings", currentView: $currentView)
+                .onTapGesture {
+                     self.currentView = "settings"
+                }
+            
             }
             .frame(maxWidth: .infinity)
-            .background(Color(red: 28/255, green: 28/255, blue: 36/255))
+            //.background(Color(red: 28/255, green: 28/255, blue: 36/255))
         
     }
 }
 
 struct TabBarView_Previews: PreviewProvider {
     static var previews: some View {
-        TabBarView()
+        TabBarView(currentView: Binding.constant("home"))
     }
 }
